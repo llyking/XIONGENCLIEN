7ac87c11a281e3336cd586a3cf894d83cf576742
Ios <Ios@IosdeMac-mini.local>
2018-03-19 14:32:07 +0800

    Initial Commit

---
+++ XIONGENCLIEN/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XIONGENCLIEN
+//
+//  Created by Ios on 2018/3/19.
+//  Copyright © 2018年 Ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
